f58189a2116c0a19d838d2b8eb977bd6d9d735b5
Maria Dominguez <ro.antarez@gmail.com>
2023-10-26 20:52:46 -0300

    javascript

--- tickets.js
+++ tickets.js
@@ -1,1 +1,50 @@
-console.log("hola")
+console.log("hola");
+
+let inputNombre = document.getElementById("nombre");
+let inputApellido = document.getElementById("apellido");
+let inputEmail = document.getElementById("email");
+let inputCantidad = document.getElementById("cantidad");
+let inputCategoria = document.getElementById("categoria");
+let botonResumen = document.getElementById("resumen");
+let botonBorrar = document.getElementById("borrar");
+
+let total = 0;
+let advertencia = "Ingresar ";
+
+botonResumen.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if("" == inputNombre.value){
+        document.getElementById('advertencia').innerHTML = advertencia + "nombre";
+    } else if("" == inputApellido.value){
+        document.getElementById('advertencia').innerHTML = advertencia + "apellido";
+    } else if("" == inputEmail.value){
+        document.getElementById('advertencia').innerHTML = advertencia + "email";
+    } else if(!emailRegex.test(inputEmail.value)){
+        document.getElementById('advertencia').innerHTML = advertencia + "un email valido";
+    } else if("" == inputCantidad.value){
+        document.getElementById('advertencia').innerHTML = advertencia + "cantidad de entradas";
+    } else {
+        document.getElementById('advertencia').innerHTML = "";
+        if("Estudiante" == inputCategoria.value){
+            total = parseInt(inputCantidad.value) * 40
+            document.getElementById('total').innerHTML = total;
+        }
+        if("Trainee" == inputCategoria.value){
+            total = parseInt(inputCantidad.value) * 100
+            document.getElementById('total').innerHTML = total;
+        }
+        if("Junior" == inputCategoria.value){
+            total = parseInt(inputCantidad.value) * 170
+            document.getElementById('total').innerHTML = total;
+        }
+    }
+
+});
+
+botonBorrar.addEventListener('click', (e) => {
+    total = 0;
+    document.getElementById('total').innerHTML = total;
+})
